cb73640bdc9ee25a9a9e8b51502cba09c0728367
Frank Asefuah <asefuahfrank@gmail.com>
2023-03-28 16:20:13 +0000

    Job Title, Company, Location and Link to full job

--- main.js
+++ main.js
@@ -24,14 +24,6 @@ fetch("https://www.arbeitnow.com/api/job-board-api", requestOptions)
         console.log(result.data[0].url)
 
         addElement(result)
-        // const sectionElement = document.getElementById('mainSection')
-        // const h3Tag = document.createElement('h3')
-        // console.log(h3Tag)
-        // const jobTitleContent = document.createTextNode(result.data[0].title)
-        // h3Tag.appendChild(jobTitleContent)
-        // document.body.insertBefore(sectionElement, h3Tag)
-        // sectionElement.appendChild(h3Tag)
-
     })
     .catch(error => console.log('error', error));
 
@@ -41,19 +33,34 @@ function addElement(result) {
     // create a new section element
     const newSection = document.createElement("section");
 
-    // create an h3 element
+    // create elements
     const newH3 = document.createElement('h3')
+    const newH4 = document.createElement('h4')
+    const newPar = document.createElement('p')
+    // const aTag = document.createElement('a')
 
     // and give it some content
-    const jobTitle = document.createTextNode(result.data[0].title);
+    const jobTitle = document.createTextNode(result.data[0].title)
+    const companyName = document.createTextNode(result.data[0].company_name)
+    const jobLocation = document.createTextNode(result.data[0].location)
 
-    // add the job title node to the newly created h3
-    newH3.appendChild(jobTitle);
+    // const jobUrl = document.createAttribute('href')
+    // jobUrl.value = result.data[0].url
+    // aTag.setAttribute(jobUrl)
+    // const viewJobText = document.createTextNode('View Job')
+
+    // add the content to the newly created tags
+    newH3.appendChild(jobTitle)
+    newH4.appendChild(companyName);
+    newPar.appendChild(jobLocation);
+    // aTag.appendChild(viewJobText)
 
     // add the h3 to the new section
     newSection.appendChild(newH3)
+    newSection.appendChild(newH4)
+    newSection.appendChild(newPar)
 
     // add the newly created element and its content into the DOM
-    const currentSection = document.getElementById("mainSection");
-    document.body.insertBefore(newSection, currentSection);
+    const currentSection = document.getElementById("mainSection")
+    document.body.insertBefore(newSection, currentSection)
 }
